d571270d0de55e947fb70a8c0f806ee7b7214f8a
cortensupplier <cortensupplier@gmail.com>
2026-09-18 17:02:01 +0800

    Update site-config.js

--- assets/site-config.js
+++ assets/site-config.js
@@ -1,6 +1,6 @@
 window.DAZHI_CONFIG = Object.freeze({
   company: 'SHANDONG DAZHI',
-  email: 'cs@jiujianggrp.com',
+  email: 'cortensupplier@gmail.com',
   phone: '+86 150 9506 3690',
   whatsapp: '8615095063690',
   location: 'Shandong, China',
