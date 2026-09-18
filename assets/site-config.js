f06a72cc5ff8ed9258bc0db2f9303d2d089a62b5
cortensupplier <cortensupplier@gmail.com>
2026-09-18 16:03:45 +0800

    Add files via upload

---
+++ assets/site-config.js
@@ -0,0 +1,9 @@
+window.DAZHI_CONFIG = Object.freeze({
+  company: 'SHANDONG DAZHI',
+  email: 'cs@jiujianggrp.com',
+  phone: '+86 150 9506 3690',
+  whatsapp: '8615095063690',
+  location: 'Shandong, China',
+  address: '',
+  siteUrl: ''
+});
